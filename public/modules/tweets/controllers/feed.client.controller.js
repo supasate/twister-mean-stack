65693fb6305026ed1c5ae73f8777425eda34a920
Supasate Choochaisri <supasate.c@gmail.com>
2015-09-30 03:29:06 +0700

    Able to post tweet and sort by tweetTime

--- public/modules/tweets/controllers/feed.client.controller.js
+++ public/modules/tweets/controllers/feed.client.controller.js
@@ -15,23 +15,38 @@ angular.module('tweets').controller('FeedController', [
             {
                 name: 'Arnupharp Viratanapanu',
                 screenName: 'topscores',
-                tweetText: 'My name is Top'
+                tweetText: 'My name is Top',
+                tweetTime: '2015-01-29T15:37:26+07:00'
             },
             {
                 name: 'Supasate Choochaisri',
                 screenName: 'kaizerwing',
-                tweetText: 'Hello World'
+                tweetText: 'Hello World',
+                tweetTime: '2015-02-29T15:37:26+07:00'
             },
             {
                 name: 'Somchai Jaidee',
                 screenName: 'somchaic',
-                tweetText: '555555555555555'
+                tweetText: '555555555555555',
+                tweetTime: '2015-06-29T15:37:26+07:00'
             },
             {
                 name: 'Supasate Choochaisri',
                 screenName: 'kaizerwing',
-                tweetText: 'Sawasdee Thailand'
+                tweetText: 'Sawasdee Thailand',
+                tweetTime: '2015-02-29T15:37:26+07:00'
             }
         ];
+
+        $scope.postTweet = function(tweetText, name, screenName) {
+            $scope.timeline.push({
+                name: name,
+                screenName: screenName,
+                tweetText: tweetText,
+                tweetTime: new Date().toISOString()
+            });
+
+            $scope.tweetText = '';
+        };
     }
 ]);
